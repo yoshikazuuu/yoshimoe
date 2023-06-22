70cb9856c62c66d163e3397510a7d29125b5c7a2
Yoshikazuuu <febrianojerry@gmail.com>
2023-06-22 18:58:45 +0700

    fix(random.js): change port variable value from 3000 to 3001

--- random.js
+++ random.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get("/random-image", (req, res) => {
   fs.readdir("public/img", (err, files) => {
